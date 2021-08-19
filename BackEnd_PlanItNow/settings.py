a7de7003c7ff562db1908da6d1187c48800295bb
Eduardo P <zaexv@hotmail.com>
2021-08-19 16:49:56 +0200

    feat: Added Deploy Configurations
    Modified Schema in order to deploy the master branch in heroku

--- BackEnd_PlanItNow/settings.py
+++ BackEnd_PlanItNow/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+import decouple
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -23,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '4guo7(5ehg9q+11tlihq7d9zi3c=e#v+(f#$0um$d*=56d9#zk'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*'] #TODO Change
 
@@ -80,11 +82,21 @@ WSGI_APPLICATION = 'BackEnd_PlanItNow.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
+
+import dj_database_url
+from decouple import config
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL')
+    )
 }
 
 
