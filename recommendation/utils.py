483ac795139b006502c5ddd7fc05198d15f4896e
Eduardo P <zaexv@hotmail.com>
2021-09-07 23:12:09 +0200

    feat: UserDistance model to store distance between plans and users
    It has been added a new Django model

--- recommendation/utils.py
+++ recommendation/utils.py
@@ -1,6 +1,7 @@
 """De momento este utils va a ser una forma de probar scripts. Ya lo cambiaré más adelanteG"""
 
 from deep_translator import GoogleTranslator
+from deep_translator.exceptions import NotValidPayload
 
 from plans.models import Plan
 ### PLN ###
@@ -22,15 +23,20 @@ from nltk.corpus import wordnet as wn
 
 
 
-def translateAllPlans():
+def translate_all_plans():
     for plan in Plan.objects.all():
-        print("\t Traduciendo plan...{plan.id}")
-        english_title = GoogleTranslator(source='auto', target='en').translate(text=plan.title+"")
+        print("\t Traduciendo plan...{plan}")
+        try:
+            english_title = GoogleTranslator(source='auto', target='en').translate(text=plan.title+"")
+            english_description = GoogleTranslator(
+                source='auto', target='en').translate(text=plan.description)
+            print(english_description)
+        except NotValidPayload:
+            english_title='Not valid'
+            english_description='Not valid'
         print(english_title)
 
-        english_description = GoogleTranslator(
-            source='auto', target='en').translate(text=plan.description)
-        print(english_description)
+
 
         plan.english_title = english_title
         plan.english_description = english_description
@@ -50,17 +56,16 @@ def get_lemmas(text):
         "n't", "'m", "'s", ':', '...', '!', '?', ',',
         'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
         'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
-        'thanks', 'let'
+        'thanks', 'let' , 'ta'
     ]
-
-
     text = text.lower()
     english_words = words.words()
     all_words = word_tokenize(text)
 
     is_noun = lambda pos: pos[:2] == 'NN'
-    is_verb = lambda pos: pos[:2] == 'VBP' #| pos[:2] == 'VB'
-    nouns = [word for (word, pos) in nltk.pos_tag(all_words) if (is_noun(pos) | is_verb(pos))]
+    is_verb = lambda pos: pos[:2] == 'VB'
+    is_verb_p = lambda pos: pos[:2] == 'VBP'
+    nouns = [word for (word, pos) in nltk.pos_tag(all_words) if (is_noun(pos) | is_verb(pos) | is_verb_p(pos))]
 
     text_without_stop = ''
 
@@ -69,13 +74,20 @@ def get_lemmas(text):
         is_english_word = (word not in custom_stopwords) and (word in english_words)
         if (is_english_word):
             text_without_stop = text_without_stop + word + ' '
-
     words_without_stop = word_tokenize(text_without_stop);
-
     lemmatizer = WordNetLemmatizer()
-
-
     # Ver cómo lematizar según categoría
     lemmas = words_lemma = [lemmatizer.lemmatize(word) for word in words_without_stop]
-
     return lemmas
+
+def assign_all_plans_lemmas():
+    for plan in Plan.objects.all():
+        title_lemmas = get_lemmas(plan.english_title)
+        description_lemmas = get_lemmas(plan.english_description)
+        print(title_lemmas)
+        print(description_lemmas)
+        print(plan.title)
+        print(plan.description)
+
+def calculate_distance_between_plans(plan1, plan2):
+    pass
